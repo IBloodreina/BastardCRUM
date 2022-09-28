fa31a9d65d9a763f348767221fd9c40e7289a83e
Magnus <magnusgronhult@gmail.com>
2022-09-28 15:40:37 +0200

    CRUM 15:40

--- 1.js
+++ 1.js
@@ -1,42 +1,96 @@
-const cheeseMeal = ["cheese", "cheezy dressing", "tomato"]
-const spicyBurger = ["cheese", "cheezy dressing", "tomato"]
-const veganTexas = ["vegan cheese", "chipotle dressing", "jalapeno"]
-
-const wrap = document.querySelector(".wrap")
+const doubleCheese = ["120 gram nöttkött", "cheezy dressing", "tomato"]
+const london = ["120 gram nötkött", "originalddressing", "tomato"]
+const manhattan = ["120 gram sojabönor", "chipotle dressing", "jalapeno"]
+const newYork = ["120 gram nötkött", "sallad", "tomat", "rödlök", "dubbel ost"]
+const texas = ["2x120 gram nötkött", "sallad", "jalapeno", "rödlök", "cheddar"]
 
 const burgerData = [
     // { hamburgerCode: "01", hamburgerName: "Cheese meal", ingredients: {cheeseMeal}},
-    { hamburgerCode: "01", hamburgerName: "Cheese meal", cheeseMeal},
-    { hamburgerCode: "02", hamburgerName: "Spicy meal", spicyBurger},
-    { hamburgerCode: "03", hamburgerName: "Vegan texas meal", veganTexas},
+    { hamburgerCode: "01", hamburgerName: "Double cheese", ingredients: doubleCheese},
+    { hamburgerCode: "02", hamburgerName: "London", ingredients: london},
+    { hamburgerCode: "03", hamburgerName: "Manhattan Madness", ingredients: manhattan},
+    { hamburgerCode: "04", hamburgerName: "New York", ingredients: newYork},
+    { hamburgerCode: "05", hamburgerName: "Texas YiHaa", ingredients: texas},
 ]
- 
+
 const hamburger = document.querySelector(".bastard .hamburger") 
 const ingredients = document.querySelector(".bastard .ingredients")
- 
+const burgerPicture = document.querySelector(".bastard img.Burger")
+
+
 renderBurgers()
 
-
-
- 
 function renderBurgers () {
     let html = `<option value="">Välj burgare...</option>`
- 
+
     for(let c of burgerData) {
         const name = c.hamburgerName
         const code = c.hamburgerCode
- 
+
         html += `<option value="${code}">${name}</option>`
         
     }
     hamburger.innerHTML = html
+
 }
- 
+
+
 function burgerChanged() {
     renderIngredients()
-    
- 
 }
+
+
+function renderIngredients() {
+
+    let html= ""
+
+    const selectedBurger = burgerData.find(b => b.hamburgerCode === hamburger.value) 
+
+    for (let m of selectedBurger.ingredients) {
+        console.log(m)
+
+        html += `<li value="">${m}</li>`
+
+    }
+    
+    ingredients.innerHTML = html
+
+    let hcode = selectedBurger.hamburgerCode
+
+    burgerPictureChange(hcode) 
+
+}
+
+function burgerPictureChange(hcode) {
+
+    if (hcode === "01") {
+        burgerPicture.src = "burgers/doublecheese.png"
+        return
+    }
+
+    if (hcode === "02") {
+        burgerPicture.src = "burgers/london.png"
+        return
+    }
+
+    if (hcode === "03") {
+        burgerPicture.src = "burgers/manhattan.png"
+        return
+    }
+    
+    if (hcode === "04") {
+        burgerPicture.src = "burgers/newyork.png"
+        return
+    }
+
+    if (hcode === "05") {
+        burgerPicture.src = "burgers/texas.png"
+        return
+    }
+    
+}
+
+
 
 ShowMenu()
 {
